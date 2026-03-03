58098226333bb25cb92cdb266fbdbd7ca71fa276
Yonier <yasprill@bancolombia.com.co>
2026-03-03 02:16:38 -0500

    chore: bump formula to v1.4.1

--- Formula/ksw.rb
+++ Formula/ksw.rb
@@ -1,8 +1,8 @@
 class Ksw < Formula
   desc "AI-powered interactive Kubernetes context switcher"
   homepage "https://github.com/YonierGomez/ksw"
-  url "https://github.com/YonierGomez/ksw/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "58e7d4834064647717145aed9561b3e495ef550b8ce417904b7322fde54e4a4f"
+  url "https://github.com/YonierGomez/ksw/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "40ecfc8b60a357d6b965ed268fe500a22c86e3e5560900fafd0cded515d5c991"
   license "MIT"
 
   depends_on "go" => :build
